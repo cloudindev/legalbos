f9243a41b3bda50194ee8b27605b05eadc26b812
muyfresh <muyfreshbos@gmail.com>
2026-03-03 21:10:36 +0100

    chore: remove test scripts

--- test-bcrypt.js
+++
@@ -1,3 +0,0 @@
-const bcrypt = require("bcryptjs");
-console.log(typeof bcrypt.compare);
-import("bcryptjs").then(b => console.log("import:", typeof b.compare, "default:", typeof b.default?.compare));
